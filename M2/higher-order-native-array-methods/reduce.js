22464744772eb648bf68495599538f57651de8be
bowersjuan <bowersjuan@gmail.com>
2022-03-10 14:43:14 -0500

    Update reduce method

--- M2/higher-order-native-array-methods/reduce.js
+++ M2/higher-order-native-array-methods/reduce.js
@@ -81,10 +81,10 @@ let sum1 = numbers2.reduce((acc, el) => {
 console.log(sum1); // => 10
 
 // validate our element values are correct
-let numbers3 = ['string'];
+let numbers3 = ["string"];
 
 let sumNumbers = numbers3.reduce((acc, el) => {
-  if (typeof el !== 'number') {
+  if (typeof el !== "number") {
     return 0;
   }
   return acc + el;
@@ -96,7 +96,7 @@ console.log(sumNumbers);
 let numbers4 = [];
 
 let sumNumbers4 = numbers4.reduce((acc, el) => {
-  if (typeof el !== 'number') {
+  if (typeof el !== "number") {
     return 0;
   }
   return acc + el;
@@ -146,10 +146,13 @@ let sum3 = arr2.reduce(adder, 5);
  */
 
 function stringConcat(arr) {
-  // your code here
+  let concatinatedString = arr.reduce((currentElement, nextElement) => {
+    return currentElement.toString() + nextElement.toString();
+  });
+  return concatinatedString;
 }
 
-// console.log(stringConcat([1, 2, 3])); // "123"
+console.log("stringConcat", stringConcat([1, 2, 3])); // "123"
 /**
  * 5
  *
@@ -159,24 +162,32 @@ function stringConcat(arr) {
  */
 
 function totalVotes(arr) {
-  // your code here
+  let votes = arr.reduce((acc, nextVote) => {
+    if (nextVote.voted) {
+      return ++acc;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  return votes;
 }
 
 const voters = [
-  { name: 'Bob', age: 30, voted: true },
-  { name: 'Jake', age: 32, voted: true },
-  { name: 'Kate', age: 25, voted: false },
-  { name: 'Sam', age: 20, voted: false },
-  { name: 'Phil', age: 21, voted: true },
-  { name: 'Ed', age: 55, voted: true },
-  { name: 'Tami', age: 54, voted: true },
-  { name: 'Mary', age: 31, voted: false },
-  { name: 'Becky', age: 43, voted: false },
-  { name: 'Joey', age: 41, voted: true },
-  { name: 'Jeff', age: 30, voted: true },
-  { name: 'Zack', age: 19, voted: false },
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
 ];
-// console.log(totalVotes(voters)); // 7
+console.log("totalVotes", totalVotes(voters)); // 7
 
 /**
  * 6
@@ -185,18 +196,22 @@ const voters = [
  */
 
 function shoppingSpree(arr) {
-  // your code here
+  let totalCost = arr.reduce((acc, nextItem) => {
+    return acc + nextItem.price;
+  }, 0);
+
+  return totalCost;
 }
 
 const wishlist = [
-  { title: 'Tesla Model S', price: 90000 },
-  { title: '4 carat diamond ring', price: 45000 },
-  { title: 'Fancy hacky Sack', price: 5 },
-  { title: 'Gold fidgit spinner', price: 2000 },
-  { title: 'A second Tesla Model S', price: 90000 },
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
 ];
 
-// console.log(shoppingSpree(wishlist)); // 227005
+console.log("shoppingSpree", shoppingSpree(wishlist)); // 227005
 /**
  * 7
  *
@@ -206,12 +221,16 @@ const wishlist = [
  */
 
 function flatten(arr) {
-  // your code here
+  let flattenedArr = arr.reduce((acc, nextArray) => {
+    return acc.concat(nextArray);
+  }, []);
+
+  return flattenedArr;
 }
 
-const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 
-// console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+console.log("flatten:", flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
 
 /**
  * 8
@@ -221,11 +240,9 @@ const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
  * Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
  */
 
-function voterResults(arr) {
-  // your code here
-}
+function voterResults(arr) {}
 
-// console.log(voterResults(voters)); // Returned value shown below:
+console.log(voterResults(voters)); // Returned value shown below:
 /*
 { numYoungVotes: 1,
   numYoungPeople: 4,
