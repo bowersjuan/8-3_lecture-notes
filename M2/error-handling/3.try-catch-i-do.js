121f8a7d3c9e785c2eeb2f926c01e8c625b00f7e
bowersjuan <bowersjuan@gmail.com>
2022-03-11 11:52:53 -0500

    Update try-catch

--- M2/error-handling/3.try-catch-i-do.js
+++ M2/error-handling/3.try-catch-i-do.js
@@ -1,11 +1,9 @@
 function guestIsValid(guest) {
-  let missingFields = [];
-  if (!guest.name) {
-    missingFields.push("name");
-  }
-  if (!guest.email) {
-    missingFields.push("email");
-  }
+  guest.notARealMethod();
+  let requiredFields = ["name", "email"];
+  let missingFields = requiredFields.filter(
+    (field) => !guest.hasOwnProperty(field)
+  );
 
   if (missingFields.length) {
     throw `The following fields are required: ${missingFields.join(", ")}`;
@@ -16,7 +14,12 @@ function guestIsValid(guest) {
 
 /** 1. How can you handle errors that may occur in a program you've written? */
 
-guestIsValid({});
+try {
+  console.log("guest is valid", guestIsValid({}));
+  console.log("guest is valid", guestIsValid({}));
+} catch (err) {
+  console.log(" WE CAUGHT A WILD ERROR!", err);
+}
 
 /** 2. How is a try/catch block similar to an if/else statement? */
 
